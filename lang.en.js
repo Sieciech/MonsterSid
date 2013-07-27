7ee3d6492d5cb4d2207fd4c52b259e8ae2005b03
Sieciech <michal@freev.net>
2013-07-27 19:11:57 +0200

    Create lang.en.js

---
+++ lang.en.js
@@ -0,0 +1,22 @@
+MonsterSid.translates = {
+  PolicyTitle 'Cookies Policy',
+  PolicyDesc: 'We use cookies to facilitate your use of our website. Staying on the page you agree to the use of cookies. '
+  PolicyAccept: 'I accept',
+  PolicyMore: 'More Information',
+  DisableTitle: 'What are cookies?'
+  DisableBrowser: 'How to disable cookies in your browser ${browser}'
+  DisableFirefox: 'In the "Tools" menu, select "Options" and their "Privacy" tab. <br> Browser gives you the opportunity to indicate that they do not want to be tracked at all or remove individual cookies each site.'
+  DisableMSI: 'In the "Tools" menu, select "Internet Options" and their "Privacy" tab. <br> Special slider you can adjust the overall level of privacy or the "Sites" manage settings for individual websites.'
+  DisableChromium: 'The hidden menu in three horizontal stripes, the upper right corner of the browser, select "Tools" and then "Clear browsing data ...". In addition to cleaning the cookies, there is a link "More information" that leads to a detailed description of the function of your browser's privacy. '
+  DisableOpera: 'With the "Opera" in the upper left corner, open the menu and select the "Settings" tab and the "Clear browsing data ...". <br> Addition to delete cookies already set, there is also the "Manage cookies. .. " leading to more advanced options for each site. '
+  DisableSafari: 'In the "Safari" menu and select "Preferences" and their "Privacy" tab. You will find a number of options for cookies. '
+  DisableMobile: 'Each phone model can serve this function in a different way. Therefore, we encourage you to read the privacy options in the documentation on the website of the manufacturer of your mobile device. '
+  DisableDesc: 'Many websites save to your computer, and more specifically in the glove specific browsers (Firefox, Internet Explorer, Chrome, etc.) on your account by phone or a computer on which you are connecting with the network, called. cookies. <br> Typically cookies are very useful. With him are saved to your personal settings and thanks to them you can login to your mail. <br> Authors websites so it knows how many visitors to their peers and what exactly they read, and who walk away. <br> Information collected by the use of cookies may also be used to collect information about you for commercial purposes. For example, if you enter the site frequently about books, you\'ll see more ads bookstore. <br> Fortunately, if you believe that the presence of cookies violate your privacy, you can always turn it off, or for a particular site or in general for all connections your web browser. '
+  DisablePluginTitle: 'How to automatically accept cookies?',
+  DisablePluginDesc: '<span onclick="MonsterSid.bccpet(this);" style="text-shadow:none;display:inline-block;font-size:14px;border:1px solid #888;background:#def;padding:2px 6px; cursor:pointer;border-radius:4px;" onmouseover="this.style.background=\'#888\';this.style.color=\'#fff\';" onmouseout="this.style.background=\'#def\';this.style.color=\'#000\'">${accept}</span> You can accept cookies in other pages now and no longer see similar notifications.',
+  DisablePluginAccept: 'Accept',
+  DisablePluginReject: 'Cancel',
+  LanguegeSet: 'is set into English',
+  CookiesPolicyAccepted: 'policies have been accepted cookies',
+  ErrorAcceptAll: 'Error, try for a while',
+};
